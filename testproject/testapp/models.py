747777588c4664e3ab08e298b55613645fdaeea6
alexnegrya <alexnegrya@mail.ru>
2022-02-28 18:37:11 +0200

    were written models and have been applied migrations

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(unique=True, max_length=100, null=False, blank=False)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='images/', max_length=100)
+    creator = models.CharField(max_length=100)
+    create_date = models.DateTimeField(default=timezone.now())
+
+
+class JSON(models.Model):
+    content = models.JSONField()
